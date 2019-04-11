3a624541d7d366a49fa21cb2b663188f42c4664c
Ryan Woo <devrwoo@gmail.com>
2019-04-10 23:24:34 -0700

    Initial Commit

---
+++ index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const name = "Ryan", age = 25, gender = 'Male';
+const sayHi = (name, age, gender) => {
+    console.log(`Hello ${name}, you are ${age} and ${gender}`);
+};
+sayHi(name, age, gender);
+//# sourceMappingURL=index.js.map
---
+++ index.ts
@@ -0,0 +1,12 @@
+
+const name = "Ryan",
+age = 25,
+gender = 'Male';
+
+const sayHi = (name, age, gender) => {
+    console.log(`Hello ${name}, you are ${age} and ${gender}`);
+}
+
+sayHi(name, age, gender);
+
+export {};
